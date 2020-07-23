3a0eef05a3eddb1d1ec5c175869642526c57d4ac
Taylor <taylorhcarroll1@gmail.com>
2020-07-23 12:57:44 -0500

    adding

---
+++ com.taylorcarroll.test2/src/main/java/com/taylorcarroll/test2/App.java
@@ -0,0 +1,34 @@
+package com.taylorcarroll.test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //var type holds up to 300 figures
+        double touchdown;
+        touchdown = 1.5;
+        //var type holds 38 figures
+        float touchdown2;
+        touchdown2 = 2;
+        int touchdown3;
+        touchdown3 = 3;
+
+        char letter = 'L';
+        String sentence = "This is a sentence, strings don't like single quotes, use a double quote.";
+        boolean homerun = false;
+
+
+        System.out.println(touchdown);
+        System.out.println(touchdown2);
+        System.out.println(touchdown3);
+        //prints 'L'
+        System.out.println(letter);
+        System.out.println(sentence);
+        System.out.println(homerun);
+    }
+}
